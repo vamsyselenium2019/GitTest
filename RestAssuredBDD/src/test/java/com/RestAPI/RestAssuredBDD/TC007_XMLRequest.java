282e2836d55d7a205b256f502fc50e99f362f9b6
vamsyselenium2019 <vamsyselenium2019@gmail.com>
2020-07-19 12:54:24 +0530

    commit

--- RestAssuredBDD/src/test/java/com/RestAPI/RestAssuredBDD/TC007_XMLRequest.java
+++ RestAssuredBDD/src/test/java/com/RestAPI/RestAssuredBDD/TC007_XMLRequest.java
@@ -20,7 +20,7 @@ public class TC007_XMLRequest {
 			.log().all();
 			
 
-		System.out.println("hai krishna hello vamsi krisshnasdadsad");
+		System.out.println("hai krishna hello vamsi krisshnasdadsad asadasd");
 
 
 	}
