1ebf78c86c57c55cc5ff9f5287567323c804f2c3
vamsyselenium2019 <vamsyselenium2019@gmail.com>
2020-07-19 12:19:48 +0530

    vamsi first commit

--- RestAssuredBDD/src/test/java/com/RestAPI/RestAssuredBDD/TC007_XMLRequest.java
+++ RestAssuredBDD/src/test/java/com/RestAPI/RestAssuredBDD/TC007_XMLRequest.java
@@ -19,6 +19,6 @@ public class TC007_XMLRequest {
 			//.body(hasXPath("/CUSTOMER/ID"), equalTo("15"))
 			.log().all();
 		
-		System.out.println("hai");
+		System.out.println("hai hello vamsi");
 	}
 }
